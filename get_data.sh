9c0ee4d525460b2d6c6f2dec5aad099aa837efc2
lukbrb <lucasbarbr@outlook.com>
2023-11-09 20:58:28 +0100

    ajout min et max 0514

--- get_data.sh
+++ get_data.sh
@@ -1,7 +1,17 @@
 #!/bin/bash
 
 # Note: avoid headers to easily open with numpy and plot
-sqlite3 -csv data/weatherdata.db "select * from max_record_15;" > data/max_records_15.csv
+sqlite3 -header -csv data/weatherdata.db "select * from max_record_15;" > data/max_records_15.csv
 echo "Résultats écrits dans le fichier 'data/max_records_15.csv'"
-sqlite3 -csv data/weatherdata.db "select * from min_record_15;" > data/min_records_15.csv
+
+sqlite3 -header -csv data/weatherdata.db "select * from min_record_15;" > data/min_records_15.csv
 echo "Résultats écrits dans le fichier 'data/min_records_15.csv'"
+
+sqlite3 -header -csv data/weatherdata.db "select * from max_0514;" > data/max_0514.csv
+echo "Résultats écrits dans le fichier 'data/max_0514.csv'"
+
+sqlite3 -header -csv data/weatherdata.db "select * from min_0514;" > data/min_0514.csv
+echo "Résultats écrits dans le fichier 'data/min_0514.csv'"
+
+sqlite3 -header -csv data/weatherdata.db "select * from min_0514;" > data/min_0514.csv
+echo "Résultats écrits dans le fichier 'data/min_0514.csv'"
