03bd39099dd3ce94c47a537f9194f34a7a203ce1
lukbrb <lucasbarbr@outlook.com>
2023-11-09 20:24:33 +0100

    enleve headers

--- get_data.sh
+++ get_data.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
-sqlite3 -header -csv data/weatherdata.db "select * from max_record_15;" > data/max_records_15.csv
+# Note: avoid headers to easily open with numpy and plot
+sqlite3 -csv data/weatherdata.db "select * from max_record_15;" > data/max_records_15.csv
 echo "Résultats écrits dans le fichier 'data/max_records_15.csv'"
-sqlite3 -header -csv data/weatherdata.db "select * from min_record_15;" > data/min_records_15.csv
+sqlite3 -csv data/weatherdata.db "select * from min_record_15;" > data/min_records_15.csv
 echo "Résultats écrits dans le fichier 'data/min_records_15.csv'"
